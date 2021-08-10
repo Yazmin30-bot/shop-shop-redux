3cab34a3e2e5348e77b1f8932c10fb7f3e89c5ca
Yazmin Tinoco <ytip30@gmail.com>
2021-08-09 23:37:20 -0500

    Implement redux on category menu component

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
+import { useQuery } from '@apollo/react-hooks';
+/* import { useStoreContext } from '../../utils/GlobalState'; */
+import { useSelector, useDispatch } from 'react-redux';
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
@@ -9,7 +10,9 @@ import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+  /* const [state, dispatch] = useStoreContext(); */
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
 
   const { categories } = state;
 
