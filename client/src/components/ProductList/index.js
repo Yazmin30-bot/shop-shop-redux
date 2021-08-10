f05550898660a32dd439a65f1939ea4f69aa004b
Yazmin Tinoco <ytip30@gmail.com>
2021-08-09 23:41:21 -0500

    Implement redux on product list component

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
+/* import { useStoreContext } from '../../utils/GlobalState'; */
+import { useSelector, useDispatch } from 'react-redux';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
-
+  /* const [state, dispatch] = useStoreContext(); */
+  //implementing redux
+  const state = useSelector((state) => state)
+  const dispatch = useDispatch()
   const { currentCategory } = state;
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
