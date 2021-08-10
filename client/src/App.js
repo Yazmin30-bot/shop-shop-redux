7edabba1ea036db617f4e625f84991523485cd22
Yazmin Tinoco <ytip30@gmail.com>
2021-08-09 22:55:05 -0500

    Import apollo/react-hooks and react-redux packages on client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {
+/* import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { setContext } from '@apollo/client/link/context'; */
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import { Provider } from 'react-redux';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -14,7 +17,7 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+/* import { StoreProvider } from './utils/GlobalState'; */
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
@@ -36,6 +39,9 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
+
+
 
 function App() {
   return (
