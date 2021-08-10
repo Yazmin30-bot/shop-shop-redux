9823522e7309cff8663cffcc51ff05d48acfffc0
Yazmin Tinoco <ytip30@gmail.com>
2021-08-09 23:26:03 -0500

    Use react-redux instead of global state

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/react-hooks';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+/* import { useStoreContext } from '../utils/GlobalState'; */
+import { useSelector, useDispatch } from 'react-redux';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -15,7 +16,11 @@ import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+ /*  const [state, dispatch] = useStoreContext(); */
+  //implementing redux
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
