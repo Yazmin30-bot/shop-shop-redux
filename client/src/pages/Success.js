18df5dac82e744c52d4c543b7577b988ed929f5c
Yazmin Tinoco <ytip30@gmail.com>
2021-08-09 23:28:21 -0500

    Use apollo/react-hooks instead of apollo/client to useMutation# !

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/react-hooks';
 import Jumbotron from '../components/Jumbotron';
 import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from '../utils/helpers';
